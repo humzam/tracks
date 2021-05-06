4751140c43e208a4be6a9ff88bfc67311ead26e2
Humza Mangrio <humzam99@gmail.com>
2021-05-05 23:37:48 -0700

    Initial commit

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://25ef42228a8d.ngrok.io'
+});
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+
+const SignupScreen = ({ navigation }) => {
+    const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(AuthContext);
+
+    useEffect(() => {
+        tryLocalSignin();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <NavigationEvents onWillFocus={clearErrorMessage} />
+            <AuthForm 
+                headerText="Sign Up for Tracker"
+                errorMessage={state.errorMessage}
+                onSubmit={signup}
+                submitButtonText="Sign Up"
+            />
+            <NavLink 
+                text="Alread have an account? Sign in instead"
+                routeName="Signin"
+            />
+        </View>
+    );
+};
+
+SignupScreen.navigationOptions = () => { 
+    return {
+        headerShown: false
+    };
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250
+    }
+});
+
+export default SignupScreen;
